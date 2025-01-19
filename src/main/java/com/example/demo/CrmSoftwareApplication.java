ff69f1cbab0d58297c1c0729b80d6f821c5384c4
yogesh kumawat <yogeshkumawat17062000@gmail.com>
2025-01-19 17:02:51 +0530

    crm project created

---
+++ src/main/java/com/example/demo/CrmSoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmSoftwareApplication.class, args);
+	}
+
+}
